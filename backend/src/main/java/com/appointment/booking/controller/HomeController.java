bff722f60452186f0bb05770e60e7906f461da95
isaliperera <100031700+isalip48@users.noreply.github.com>
2026-03-16 18:09:39 +0530

    Home Controller added to provide a simple endpoint for testing if the backend is running successfully.

---
+++ backend/src/main/java/com/appointment/booking/controller/HomeController.java
@@ -0,0 +1,10 @@
+package com.appointment.booking.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "Backend is running successfully!";
+    }
+}

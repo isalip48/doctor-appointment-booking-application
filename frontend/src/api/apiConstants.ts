61235e42f767a76f53888fdc4e6c6ac1c5da1537
isaliperera <100031700+isalip48@users.noreply.github.com>
2026-03-16 18:16:40 +0530

    Change Base URL

--- frontend/src/api/apiConstants.ts
+++ frontend/src/api/apiConstants.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://localhost:8080/api";
+export const API_BASE_URL = "https://doctor-appointment-booking-application-production.up.railway.app";
 
 export const API_ENDPOINTS = {
   HOSPITALS: "/hospitals",
